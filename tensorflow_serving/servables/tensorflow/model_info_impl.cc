3cda15422ac04c293976fdaafc23cb7261124b59
Ferdinand Urban <ferdinand.urban@konicaminolta.cz>
2017-09-25 10:18:22 +0200

    minor changes

--- tensorflow_serving/servables/tensorflow/model_info_impl.cc
+++ tensorflow_serving/servables/tensorflow/model_info_impl.cc
@@ -26,7 +26,7 @@ Status ValidateModelInfoRequest(const ModelInfoRequest& request) {
 Status ModelInfoImpl::GetModelInfo( ServerCore* core, const ModelInfoRequest& request, ModelInfoResponse* response) {
   TF_RETURN_IF_ERROR(ValidateModelInfoRequest(request));
   const std::vector<ServableId> available_servables = core->ListAvailableServableIds();
-  
+
   //std::cout << "[i] GetModelInfo..." << request.model_name() << std::endl;
   if(request.model_name().empty()) {
     for (auto it = available_servables.begin() ; it != available_servables.end(); ++it) {
@@ -40,8 +40,8 @@ Status ModelInfoImpl::GetModelInfo( ServerCore* core, const ModelInfoRequest& re
         //std::cout << "[i] ModelInfoRequest.model_name ==> returning a single servables." << std::endl;
         ModelInfo *mi = response->add_servables();
         mi->set_name(servable.name);
-        mi->set_version(servable.version);		
-      } 
+        mi->set_version(servable.version);
+      }
     }
   }
 
