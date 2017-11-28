7e35014a7c54145a695a4d44849ff3485a107f50
Ferdinand Urban <ferdinand.urban@konicaminolta.cz>
2017-11-28 10:00:16 +0100

    changed path to models.conf

--- run_my_server.sh
+++ run_my_server.sh
@@ -1,1 +1,1 @@
-bazel-bin/tensorflow_serving/model_servers/tensorflow_model_server --port=11111 --model_config_file=/fastdata/serving/models.conf
+bazel-bin/tensorflow_serving/model_servers/tensorflow_model_server --port=11111 --model_config_file=/fastdata/models_repo/models.conf
